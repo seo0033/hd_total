75ef2b52202b6f4353cce47d9dcb94de0ec100d4
seo0033 <tkrhk9511@gmail.com>
2022-09-02 16:22:55 +0900

    20220902

--- javascript/js/main.js
+++ javascript/js/main.js
@@ -1,68 +1,169 @@
 window.addEventListener('DOMContentLoaded', () => {
-    ///////////////////////////////////
 
     document.querySelector('.top_close_btn').addEventListener('click', function () {
-        // this.classList.toggle('on');
-        document.querySelector('.TopBanner').classList.add('on')
-        document.querySelector('.MainVisual').classList.add('on')
+        //this.classList.toggle('on');
+        document.querySelector('.TopBanner').classList.add('on');
+        document.querySelector('.MainVisual').classList.add('on');
     });
+
+    document.querySelector('.lang strong').addEventListener('click', function () {
+        this.classList.toggle('on');
+        document.querySelector('.lang').classList.toggle('on');
+    });
+
+    document.querySelector('.top_search strong').addEventListener('click', function () {
+        this.classList.toggle('on');
+        document.querySelector('.top_search').classList.toggle('on');
+    });
+
+
+    // scroll event
+    // const SEC = document.querySelectorAll('.action');
+    // const WT=window.innerHeight;
+
+    // window.addEventListener('scroll', () => {
+    //     let sct = window.scrollY;
+    //     SEC.forEach(ele => {
+    //         let secTop = ele.offsetTop;
+    //         let secH = ele.clientHeight;
+    //         if (sct > secTop - (WT - secH) / 2 - 200) {
+    //             ele.classList.add('on');
+    //         };
+    //         // sct > secTop - (WT-secH) / 2 - 200 ? ele.classList.add('on') : e.classList.remove('on');
+    //     });
+    // });
+
+
+
 
     window.addEventListener('scroll', () => {
         let SCT = window.scrollY;
         SCT > 0
             ? document.querySelector('.Header').classList.add('on')
-            : document.querySelector('.Header').classList.remove('on')
-    })
+            : document.querySelector('.Header').classList.remove('on');
+
+    });
+
+    const slideDots = document.querySelectorAll('.slide_dots li');
 
     const MAINSLIDE = new Swiper('.main_slider', {
         loop: true,
+        slideActiveClass: 'on', // .swiper-slide-active 대신 .on을 쓸거임...
         on: {
-            init: function () {
-                console.log(this.slides.length - 2);
-
-                const Current = document.querySelector('.swiper-slide-active');
-                Current.classList.add('on');
-
-                document.querySelector('.main_slide_num').innerHTML = (this.realIndex + 1) + " / <span> " + (this.slides.length - 2);
+            slideChangeTransitionEnd: function () {
+                let count = this.realIndex; // 0 1 2
+                slideDots.forEach(it => it.classList.remove('on'))
+                slideDots[count].classList.add('on');
+                document.querySelector('.main_slider_num').innerHTML = (this.realIndex + 1) + " / <span>" + (this.slides.length - 2);
             }
         }
     });
 
-    const Totalslide = document.querySelectorAll('.swiper-slide');
-    const SlideDots = document.querySelectorAll('.slide_dots li');
-
-    MAINSLIDE.on('slideChangeTransitionEnd', function () {
-        // 1. 번호찍는거
-        console.log(this.realIndex);
-
-        // 2. 지금슬라이드(active)에 class(ex. 'on' ) 붙이는거
-        const Current = document.querySelector('.swiper-slide-active');
-        Totalslide.forEach(it => it.classList.remove('on'));
-        Current.classList.add('on')
-        console.log(Totalslide, Current, this.realIndex);
-
-        let count = this.realIndex;//0,1,2
-        SlideDots.forEach(it => it.classList.remove('on'));
-        SlideDots[count].classList.add('on');
-
-        document.querySelector('.main_slide_num').innerHTML = (this.realIndex + 1) + " / <span>" + (this.slides.length - 2);
-    });
-
-    document.querySelector('.MainVisual .slide_handler .prev').addEventListener('click', () => {
-        MAINSLIDE.slidePrev();
-    });
     document.querySelector('.MainVisual .slide_handler .next').addEventListener('click', () => {
         MAINSLIDE.slideNext();
     });
+    document.querySelector('.MainVisual .slide_handler .prev').addEventListener('click', () => {
+        MAINSLIDE.slidePrev();
+    });
 
-    SlideDots.forEach((it, idx) => {
+
+    slideDots.forEach((it, idx) => {
         it.addEventListener('click', () => {
             console.log(idx);
             MAINSLIDE.slideTo(idx + 1, 600)
         })
-    })
+    });
+
+    const PRS = new Swiper('.portfolio_right_slide', {
+        loop: true,
+        slidesPerView: 5,
+        spaceBetween: 30,
+    });
+
+
+    const PLS = new Swiper('.portfolio_left_slide', {
+        loop: true,
+        effect: "fade",
+        fadeEffect: {
+            crossFade: true
+        },
+    });
 
 
 
-    ///////////////////////////////////
+    document.querySelector('.Portfolio .slide_handler .next').addEventListener('click', () => {
+        PLS.slideNext();
+        PRS.slideNext();
+    });
+    document.querySelector('.Portfolio .slide_handler .prev').addEventListener('click', () => {
+        PLS.slidePrev();
+        PRS.slidePrev();
+    });
+
+    PLS.controller.control = PRS;
+    PRS.controller.control = PLS;
+
+    //출처: https://www.biew.co.kr/entry/Swiper-슬라이드-Swiper-2개-연동과-제어Controller [웹퍼블리싱 - 퍼블리싱 이야기 맑은커뮤니케이션:티스토리]
+    //centeredSlides: true,
+
+    const SCBOX = document.querySelectorAll('.Solution .content_box>div');
+
+    const SCS = new Swiper('.Solution .center_slider', {
+        loop: true,
+        // slidesPerView: 2,
+        spaceBetween: 100,
+        centeredSlides: true,
+        slidesPerView: "auto",
+        //width: 1200,
+        slideActiveClass: 'on', // .swiper-slide-active 대신 .on을 쓸거임...
+        on: {
+            slideChangeTransitionEnd: function () {
+                let count = this.realIndex; // 0 1 2 3 4 
+                SCBOX.forEach(it => it.classList.remove('on'))
+                SCBOX[count].classList.add('on');
+                document.querySelector('.solution_slider_num').innerHTML = "0" + (this.realIndex + 1) + "<span>  / 0" + SCBOX.length;
+            }
+        }
+
+    });
+
+
+    document.querySelector('.Solution .slide_handler .next').addEventListener('click', () => {
+        SCS.slideNext();
+    });
+    document.querySelector('.Solution .slide_handler .prev').addEventListener('click', () => {
+        SCS.slidePrev();
+    });
+
+
+
+
+
 });
+
+
+
+
+
+
+
+    // const Totalslide = document.querySelectorAll('.swiper-slide');
+
+
+    // MAINSLIDE.on('slideChangeTransitionEnd', function () {
+    //     // 1. 번호찍는거
+    //     console.log(this.realIndex);
+
+    //     // 2. 지금슬라이드(active)에 class(ex. 'on' ) 붙이는거
+    //     const Current = document.querySelector('.swiper-slide-active');
+    //     Totalslide.forEach(it => it.classList.remove('on'));
+    //     Current.classList.add('on')
+    //     console.log(Totalslide, Current, this.realIndex);
+
+    //     let count = this.realIndex;//0,1,2
+    //     SlideDots.forEach(it => it.classList.remove('on'));
+    //     SlideDots[count].classList.add('on');
+
+    //     document.querySelector('.main_slide_num').innerHTML = (this.realIndex + 1) + " / <span>" + (this.slides.length - 2);
+    // });
+
